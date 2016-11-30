c2ffbdf8e59af055e41c19c41546090238ea14c2
Jose Medina <jrmout@gmail.com>
2016-11-30 15:02:02 +0100

    Testing mosek

--- demo.m
+++ demo.m
@@ -1,19 +1,22 @@
 % Setup paths
+close all;
 setup_stable_lds;
 
 % Get trajectories from mouse
 limits = [0 100 0 100];
 data = generate_mouse_data(limits);
 
-options.solver = 'sedumi';
-options.eps_constraints = 1e-4;
-options.bias = false;
-options.verbose = 0;
-options.warning = false;
+options.solver = 'mosek';
+options.eps_constraints = 0.1;
+options.bias = true;
+options.verbose = 5;
+options.warning = true;
+options.criterion = 'mse';
 options.attractor = [0 0]';
 options.weights = ones(1,size(data,2)); % normalized weights for each sample
 
-[A, b] = estimate_stable_lds(data,options);
 
-% Plot result
-plot_streamlines(A, b, limits);
+figure(2);
+options.eps_constraints = 0;
+[A_direct, b_direct] = estimate_stable_lds(data,options);
+plot_streamlines(A_direct, b_direct, limits);
--- setup_stable_lds.m
+++ setup_stable_lds.m
@@ -11,3 +11,10 @@ userpath('clear');
 % add only the setup_stable_lds path
 addpath(genpath(setup_stable_lds_path));
 
+% 3rdParty optimizers 
+% Gurobi
+addpath('~/Dropbox/work/3rdParty/gurobi701/linux64/matlab/')
+
+% Mosek
+addpath('~/Dropbox/work/3rdParty/mosek/8/toolbox/');
+javaaddpath('/home/medina/Dropbox/work/3rdParty/mosek/8/tools/platform/linux64x86/bin/mosekmatlab.jar','-end');
