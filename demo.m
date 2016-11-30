62c32c78005536896ae4711ded846be54d507fdd
Jose Medina <jrmout@gmail.com>
2016-11-30 20:29:36 +0100

    Mosek as preferred solver

--- demo.m
+++ demo.m
@@ -5,15 +5,15 @@ setup_stable_lds;
 limits = [0 100 0 100];
 data = generate_mouse_data(limits);
 
-options.solver = 'sedumi';
-options.eps_constraints = 1e-4;
-options.bias = false;
-options.verbose = 0;
+options.solver = 'mosek';
+options.eps_constraints = 0.01;
+options.bias = true;
+options.verbose = 1;
 options.warning = false;
 options.attractor = [0 0]';
 options.weights = ones(1,size(data,2)); % normalized weights for each sample
 
-[A, b] = estimate_stable_lds(data,options);
+[A_inv, x_attractor] = estimate_stable_lds_inverse(data,options);
 
 % Plot result
-plot_streamlines(A, b, limits);
+plot_streamlines_inverse(A_inv, x_attractor, limits);
