c1c56d949ae33e47b276144fd52434b0c2563107
Jose Medina <jrmout@gmail.com>
2016-12-01 14:54:15 +0100

    Standard solver is sedumi

--- demo_mix_inv_lds.m
+++ demo_mix_inv_lds.m
@@ -8,12 +8,12 @@ n_comp = 3;
 em_iterations = 5;
 
 % Optimization options
-options.solver = 'mosek';
+options.solver = 'sedumi';
 options.eps_pos_def = 0.1;
 options.verbose = 1; 
 options.warning = 0;
 
-lambda = em_mix_lds_inverse(data, n_comp, em_iterations, options);
+lambda = em_mix_inv_lds(data, n_comp, em_iterations, options);
 
 % Plot result
 plot_streamlines_mix_lds_inv(lambda,limits);
