44895af91b3dd17adadae585d1728e922439979f
Jose Medina <jrmout@gmail.com>
2016-12-04 18:27:45 +0100

    Options as optional input and detailed comments

--- demo_inv_lds.m
+++ demo_inv_lds.m
@@ -3,17 +3,19 @@ setup_stable_lds;
 
 % Get trajectories from mouse
 limits = [0 100 0 100];
+disp('Draw a few trajectories in the figure with your mouse and press stop recording when done.')
 data = generate_mouse_data(limits);
 
-options.solver = 'mosek';
-options.eps_constraints = 0.01;
-options.bias = true;
-options.verbose = 1;
-options.warning = false;
-options.attractor = [0 0]';
-options.weights = ones(1,size(data,2)); % normalized weights for each sample
+% Optimization options
+clear options;
+options.solver = 'sedumi';
+options.eps_constraints = 0.001;        % Pos def eps margin
+options.verbose = 0;                    % Verbose (0-5)
+options.warning = false;                % Display warning information
+%options.attractor = [0 0]';             % Ignored if options.bias=true
+options.weights = ones(1,size(data,2)); % Weights for each sample
 
-[A_inv, x_attractor] = estimate_stable_inv_lds(data,options);
+[A_inv, x_attractor] = estimate_stable_inv_lds(data, options);
 
 % Plot result
-plot_streamlines_inverse(A_inv, x_attractor, limits);
+plot_streamlines_inv_lds(A_inv, x_attractor, limits);
--- estimate_stable_inv_lds.m
+++ estimate_stable_inv_lds.m
@@ -1,51 +1,69 @@
-function [A_inv, x_attractor]=estimate_stable_inv_lds(data, options, varargin)
-% ESTIMATE_STABLE_LDS_INVERSE fits a stable linear dynamical system
-% inverting the estimation problem. Instead of considering the standard
-% model
-%   x_dot = A*x + b = -A (x - x_attractor)
-% it assumes that A is positive definite and performs the regression task
-% in the inverse model, i.e.
-%   x = x_attractor - A_inv x_dot
-% 
-% this model has the benefit of having the attractor as bias. As a drawback
-% it performs worse than the standard regression procedure when it has very
-% few data points. 
+function [A_inv, x_attractor]=estimate_stable_inv_lds(data, varargin)
+%   ESTIMATE_STABLE_INV_LDS fits a stable linear dynamical system inverting
+%   the estimation problem. Instead of considering the standard model
+%       x_dot = A*x + b = -A (x - x_attractor)
+%   it assumes that A is positive definite (and possibly nonsymmetric) and 
+%   performs the regression task in the inverse model, i.e.
+%       x = x_attractor - A_inv x_dot
+%   this model has the benefit of having the attractor as bias. More
+%   precisely it solves the problem
+%   min (error^2)  subject to:  error == x_attractor - A_inv x_dot - x
+%       A                       A_inv + A_inv' >= options.eps_constraints*I
 %
-%   ESTIMATE_STABLE_LDS solves the optimization problem
-%    min (error^2)  subject to:  error == A*x + b - x_dot
-%     A                          A'+A <= -options.eps_constraints*I
-%                                options.eps_constraints*I <= P
-%   
-%   [A_OUT, b_out] = ESTIMATE_STABLE_LDS(DATA, OPTIONS) returns the system
-%   matrix and the bias of a linear dynamical system
+%   USAGE:
+%   [A_INV, X_ATTRACTOR] = ESTIMATE_STABLE_LDS_INVERSE(DATA) fits a linear
+%   dynamical system to the data and returns the inverse of the system matrix
+%   and the estimated attractor.
+%
+%   [A_INV, X_ATTRACTOR] = ESTIMATE_STABLE_LDS_INVERSE(DATA, OPTIONS) fits a 
+%   linear dynamical system to the data with the specified options 
 %   
 %   INPUT PARAMETERS:
 %   -data    data = [x; x_dot] and size(data) = [d*2,n_data_points], 
 %            where d is the dimenstion of the input/output.
+%
+%   Optional input parameters
 %   -options options.solver -- specifies the YALMIP solver.
 %            options.eps_constraints -- specifies the eps for the
 %                                       constraints
-%            options.bias (true|false) -- specifies if the LDS is 
-%                                         x_dot = A*x + b (true) or just 
-%                                         x_dot = A*x (false)
-%            options.attractor         -- specifies a priori the atractor
-%            options.weights           -- weighting factor for each sample
+%            options.attractor       -- specifies a priori the atractor
+%            options.weights         -- weighting factor for each sample
 %
 %   OUTPUT PARAMETERS:
-%   - A_out  estimated system matrix
-%   - b_out  estimated bias
-%
+%   - A_inv  estimated inverse system matrix
+%   - x_attractor  estimated attractor
 %
 %   # Author: Jose Medina
 %   # EPFL, LASA laboratory
 %   # Email: jrmout@gmail.com
 
-d=size(data,1)/2;
+% Check for options
+if nargin > 1
+    options = varargin{1};
+else
+    options = [];
+end
+
+% Default values
+if ~isfield(options, 'solver')
+    options.solver = 'sedumi';
+end 
+if ~isfield(options, 'verbose')
+    options.verbose = 0;
+end 
+if ~isfield(options, 'warning')
+    options.warning = 0;
+end 
+if ~isfield(options, 'eps_constraints')
+    options.eps_constraints = 1e-3;
+end 
+
 options_solver=sdpsettings('solver',options.solver, ...
                            'verbose', options.verbose, ...
                            'warning', options.warning);
 
 % Solver variables
+d=size(data,1)/2;
 A_inv = sdpvar(d,d,'full');
 x_star = sdpvar(d,1);
 error = sdpvar(d,size(data,2));
@@ -54,13 +72,13 @@ if isfield(options, 'weights')
     objective_function=sum(options.weights.*(sum(error.^2)));
 end
 
-% Define constraints
+% Constraints
 C=[error == -A_inv*data(d+1:2*d,:) + repmat(x_star,1,size(data,2))-data(1:d,:) ];
-% Positive definite
+% Lyapunov LMI setting P=I -> Pos def (nonsymmetric) matrix
 C = C + [A_inv + A_inv' >= options.eps_constraints*eye(d,d)] ;
 
 % Do not estimate the bias, set it to the one specified a priori
-if options.bias == false
+if isfield(options, 'attractor')
     if (size(options.attractor,1) ~= d)
         error(['The specified attractor should have size ' d 'x1']);
     else
@@ -68,7 +86,7 @@ if options.bias == false
     end
 end
 
-% Solve the optimization
+% Solve
 sol = optimize(C,objective_function,options_solver);
 if sol.problem~=0
     warning(sol.info);
