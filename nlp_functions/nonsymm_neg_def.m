01fcea9e358a18de5acb7c0f331760ba7e13e01d
Jose Medina <jrmout@gmail.com>
2016-12-08 15:30:28 +0100

    Removed bug in the transpose of the matrix

--- nlp_functions/nonsymm_neg_def.m
+++ nlp_functions/nonsymm_neg_def.m
@@ -2,7 +2,7 @@ function [c,ceq,dc,dceq]=nonsymm_neg_def(p,d,options)
 % Computes the LMI A + A' < 0 for the fminsdp solver. 
 c  = [];
 A = reshape(p(1:d*d),[d d]);
-ceq = svec(-1*(A + A)' + eye(d)*options.c_reg);
+ceq = svec(-1*(A + A') + eye(d)*options.c_reg);
 
 if nargout > 3
     dceq = zeros(d*d,numel(ceq));
