4916ba2f151d21c49cf5821886f1f545e2cb9c4d
Jose Medina <jrmout@gmail.com>
2016-12-08 12:27:01 +0100

    New fminsdp and fmincon solvers options for estimate_inv_lds

--- demo_inv_lds.m
+++ demo_inv_lds.m
@@ -8,9 +8,10 @@ data = generate_mouse_data(limits);
 
 % Optimization options
 clear options;
-options.solver = 'fminsdp';             % YALMIP solvers, e.g. 'sedumi'| 'fmincon' | 'fminsdp'
+options.solver = 'sedumi';             % YALMIP solvers, e.g. 'sedumi'|
+                                        % NLP solvers 'fmincon' | 'fminsdp'
 options.eps_pos_def = 1e-4;             % Pos def eps margin (YALMIP only)
-options.verbose = 1;                    % Verbose (0-5)
+options.verbose = 0;                    % Verbose (0-5)
 options.warning = false;                % Display warning information
 %options.attractor = [0 0]';            % Set the attractor a priori
 options.weights = ones(1,size(data,2)); % Weights for each sample
